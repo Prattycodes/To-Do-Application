2266e0471abedcd6602ba87c5d1a8e4334776b89
Prattycodes <prathamtetgure883@gmail.com>
2024-06-23 15:58:03 +0530

    Complete Almost

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react"
 
-export function CreateTodo() {
+export function CreateTodo(props) {
 
     const [title, setTitle]= useState("");
     const [description, setDescription]= useState("");
+
     return <div>
         <input style={{padding: 10, margin: 10 }} type="text" placeholder="title"
         onChange={function(e){
             const value= e.target.value;
-            setTitle(value);
+            setTitle(e.target.value);
         }}></input><br/>
         <input style={{padding: 10, margin: 10 }} type="text" placeholder="description" 
         onChange={function(e){
             const value= e.target.value;
-            setDescription(value);
+            setDescription(e.target.value);
         }}></input><br/>
 
         <button style={{padding: 10, margin: 10 }} 
